a84f7e83a1ba55ae02c201b6d922e37de2128b89
volohatameduzyaka <volohata.meduzyaka@gmail.com>
2026-05-07 09:59:32 +0300

    Update sirko.js

--- sirko.js
+++ sirko.js
@@ -2,7 +2,7 @@
     'use strict';
 
     Lampa.Utils.putScriptAsync([
-        'https://cdn.jsdelivr.net/gh/volohatameduzyaka/SirkoUA@main/SirkoCore.js?v=1'
+        'https://cdn.jsdelivr.net/gh/volohatameduzyaka/SirkoUA@main/SirkoCore.js?v=6'
     ], function () {
         console.log('Sirko loaded');
     });
