fb92eb839e13993bec173aefa1b1b37847b56909
volohatameduzyaka <volohata.meduzyaka@gmail.com>
2026-05-07 09:53:14 +0300

    Update sirko.js

--- sirko.js
+++ sirko.js
@@ -1,58 +1,10 @@
 (function () {
     'use strict';
 
-    if (window.sirko_plugin_ready) return;
-    window.sirko_plugin_ready = true;
-
-    function addButton() {
-
-        if ($('.button--sirko').length) return;
-
-        let container = $('.full-start-new__buttons');
-
-        if (!container.length) return;
-
-        container.append(`
-            <div class="full-start__button selector button--sirko">
-                <svg width="24" height="24" viewBox="0 0 24 24">
-                    <path fill="currentColor"
-                        d="M12 2L2 7V17L12 22L22 17V7L12 2Z"/>
-                </svg>
-                <span>Sirko</span>
-            </div>
-        `);
-
-        $('.button--sirko').on('hover:enter', function () {
-            Lampa.Noty.show('Sirko работает 🚀');
-        });
-
-        console.log('Sirko button added');
-    }
-
-    function init() {
-
-        console.log('Sirko init');
-
-        Lampa.Listener.follow('full', function (e) {
-
-            console.log('Sirko event', e);
-
-            if (e.type === 'complite' || e.type === 'complete') {
-
-                setTimeout(addButton, 200);
-
-            }
-
-        });
-
-    }
-
-    if (window.appready) {
-        init();
-    } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') init();
-        });
-    }
+    Lampa.Utils.putScriptAsync([
+        'https://cdn.jsdelivr.net/gh/volohatameduzyaka/SirkoUA@main/SirkoCore.js?v=1'
+    ], function () {
+        console.log('Sirko loaded');
+    });
 
 })();
