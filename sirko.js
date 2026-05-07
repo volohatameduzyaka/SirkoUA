38f3e03655e66d8a0df2261873cb9a42a3ab73c0
volohatameduzyaka <volohata.meduzyaka@gmail.com>
2026-05-07 09:56:00 +0300

    Create sirko.js

---
+++ sirko.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    Lampa.Utils.putScriptAsync([
+        'https://cdn.jsdelivr.net/gh/volohatameduzyaka/SirkoUA@main/SirkoCore.js?v=1'
+    ], function () {
+        console.log('Sirko loaded');
+    });
+
+})();
